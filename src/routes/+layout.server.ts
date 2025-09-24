8a01f2f0869651303d28d25127349cb65d95c6da
Kaspar L. Palgi <git@e-stonia.co.uk>
2025-09-24 17:58:09 +0300

    chore: hooks for error handling & redirection changes

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,21 +1,33 @@
-/** @file src/routes/+layout.server.ts */
 import { APP_ENV } from '$env/static/private';
 import { PUBLIC_API_ENV } from '$env/static/public';
+import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async (event) => {
-	const { locals } = event;
+const defaultLocale = 'en';
 
+export const load: LayoutServerLoad = async (event) => {
+	const { url, locals } = event;
 	const env = {
 		app: APP_ENV,
 		api: PUBLIC_API_ENV
 	};
-
 	const session = await locals.auth();
+
+	if (url.pathname === '/') {
+		if (!session) {
+			throw redirect(302, '/signin');
+		} else {
+			throw redirect(302, `/${defaultLocale}`);
+		}
+	}
+
+	if (url.pathname === '/auth/signin' && APP_ENV !== 'development') {
+		throw redirect(302, '/signin');
+	}
 
 	return {
 		session,
-		locale: 'en',
+		locale: defaultLocale,
 		env
 	};
 };
