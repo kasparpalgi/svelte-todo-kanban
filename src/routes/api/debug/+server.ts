5a3480b469d136077587dd350f91b52dd3d88d1a
Kaspar L. Palgi <git@e-stonia.co.uk>
2025-11-16 19:27:01 +0200

    Update +server.ts

--- src/routes/api/debug/+server.ts
+++ src/routes/api/debug/+server.ts
@@ -1,15 +1,20 @@
 import { json } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
-import { PUBLIC_APP_URL } from '$env/static/public';
+import { PUBLIC_APP_URL, PUBLIC_APP_ENV } from '$env/static/public';
 
-export async function GET() {
+export async function GET({ url }) {
 	return json({
 		PUBLIC_APP_URL,
+		PUBLIC_APP_ENV,
 		AUTH_GOOGLE_ID: env.AUTH_GOOGLE_ID?.substring(0, 20) + '...',
+		AUTH_GOOGLE_SECRET_EXISTS: !!env.AUTH_GOOGLE_SECRET,
 		AUTH_SECRET_EXISTS: !!env.AUTH_SECRET,
 		NODE_ENV: process.env.NODE_ENV,
 		ORIGIN: process.env.ORIGIN,
 		HOST_HEADER: process.env.HOST_HEADER,
-		PROTOCOL_HEADER: process.env.PROTOCOL_HEADER
+		PROTOCOL_HEADER: process.env.PROTOCOL_HEADER,
+		request_url: url.toString(),
+		request_host: url.host,
+		request_protocol: url.protocol
 	});
 }
